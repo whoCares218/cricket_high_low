caf613631f23274c5448047d33c66a38be94efa6
whoCares218 <158317888+whoCares218@users.noreply.github.com>
2025-05-11 11:12:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -107,7 +107,16 @@ function CricketHighLow() {
   };
 
   return (
+    
     <div className="game-container">
+      <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;">
+  <iframe src="https://geo.dailymotion.com/player.html?video=x81y39i"
+    style="width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden; border:none;"
+    allowfullscreen
+    title="Dailymotion Video Player"
+    allow="web-share">
+  </iframe> 
+   </div>
       <header className="game-header">
         <h1 className="game-title">Cricket High-Low Game</h1>
         <div className="score-board">
